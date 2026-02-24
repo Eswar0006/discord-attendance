8967b304b726416b2460d6ac71284ac1fc78afd9
ESWAR B <204221467+Eswar0006@users.noreply.github.com>
2026-02-24 18:08:07 +0530

    Update attendance_bot.py

--- attendance_bot.py
+++ attendance_bot.py
@@ -1,3 +1,5 @@
+from flask import Flask
+from threading import Thread
 import os
 import discord
 from discord.ext import commands
@@ -5,6 +7,18 @@ import sqlite3
 from datetime import datetime, date, time
 import pytz
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 # =========================
 # CONFIGURATION
 # =========================
@@ -120,5 +134,6 @@ async def takeattendance(ctx):
 # RUN BOT
 # =========================
 
-
+keep_alive()
 bot.run(TOKEN)
+
